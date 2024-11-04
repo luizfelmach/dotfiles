00020da37a5818f461b93f2651aa9fec7f3534ec
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-11-04 19:20:47 -0300

    update: seg 04 nov 2024 18:47:00 -03

--- .chezmoiscripts/run_after_20-docker.sh
+++ .chezmoiscripts/run_after_20-docker.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+docker_packages="$CHEZMOI_WORKING_TREE/dot_packages/docker.list"
+
 if command -v docker &> /dev/null; then
   exit 0
 fi
@@ -10,7 +12,7 @@ echo "install docker? (y/n)"
 read -r ans
 
 if [[ "$ans" == "y" ]]; then
-  sudo pacman -S docker docker-compose docker-buildx
+  sudo pacman -S $(cat $docker_packages | xargs)
   sudo usermod -aG docker $USER
   sudo systemctl start docker
   sudo systemctl enable --now docker
