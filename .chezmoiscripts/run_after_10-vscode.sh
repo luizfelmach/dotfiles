89fd1b6feed37cd122fc0c8a7c450cf9202f86b0
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-11-04 20:39:26 -0300

    update: seg 04 nov 2024 20:32:50 -03

--- .chezmoiscripts/run_after_10-vscode.sh
+++ .chezmoiscripts/run_after_10-vscode.sh
@@ -2,31 +2,11 @@
 
 set -euo pipefail
 
-extensions=(
-  "bradlc.vscode-tailwindcss"
-  "christian-kohler.path-intellisense"
-  "donjayamanne.githistory"
-  "esbenp.prettier-vscode"
-  "miguelsolorio.symbols"
-  "ms-python.debugpy"
-  "ms-python.python"
-  "ms-python.vscode-pylance"
-  "ms-toolsai.jupyter"
-  "ms-toolsai.jupyter-keymap"
-  "ms-toolsai.jupyter-renderers"
-  "ms-toolsai.vscode-jupyter-cell-tags"
-  "ms-toolsai.vscode-jupyter-slideshow"
-  "ms-vscode.cpptools"
-  "mvllow.rose-pine"
-  "natqe.reload"
-  "nomicfoundation.hardhat-solidity"
-  "pomdtr.excalidraw-editor"
-  "prisma.prisma"
-)
-
 if ! command -v code &> /dev/null; then
   exit 0
 fi
+
+mapfile -t extensions < "$CHEZMOI_WORKING_TREE/dot_extensions.list"
 
 installed=$(code --list-extensions)
 
