121f706bbff34395c458eda537eb3e5d2d603040
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-11-04 18:00:15 -0300

    update: seg 04 nov 2024 16:10:39 -03

--- .chezmoiscripts/run_after_05-yay.sh
+++ .chezmoiscripts/run_after_05-yay.sh
@@ -6,7 +6,16 @@ if command -v yay &> /dev/null; then
   exit 0
 fi
 
+echo "install yay? (y/n)"
+read -r ans
+
+if [[ "$ans" == "n" ]]; then
+  exit 0
+fi
+
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd -
+rm -rf yay
