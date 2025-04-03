35bb9e95482f3f7f39588393b5661a30983e6396
luizfelmach <luizfelmach@gmail.com>
2025-04-03 13:04:34 -0300

    clean unused files

--- .chezmoiscripts/run_after_100-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo systemctl enable sddm
-sudo systemctl enable bluetooth
-sudo systemctl enable libvirtd
-sudo systemctl enable docker
--- .chezmoiscripts/run_after_20-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-sudo usermod -aG docker $USER
